86be2170a93dc3d89e923ff3a64fdd0a4572d8ca
Elías Monzón Castro <elidarson@hotmail.com>
2019-10-24 14:13:52 -0600

    Lesson 4 finished and Lesson 5 first scaffolding.

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-list',
@@ -8,29 +9,15 @@ import { Contact } from '../contact.model';
 })
 export class ContactListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private contactService: ContactService) { }
+
   @Output() selectedContactEvent = new EventEmitter<Contact>();
 
-  contacts: Contact[] = [
-    new Contact(
-      '1',
-      'Brother Jackson',
-      'jacksonk@byui.edu',
-      '208-496-3771',
-      'https://web.byui.edu/Directory/Employee/jacksonk.jpg',
-      null
-    ),
-    new Contact(
-      '2',
-      'Bro. Barzee',
-      'barzeer@byui.edu',
-      '208-496-3768',
-      'https://web.byui.edu/Directory/Employee/barzeer.jpg',
-      null
-    )
-  ]
+  contacts: Contact[] = [];
 
   ngOnInit() {
+    //TODO: call getContacts() method from contactService
+    
   }
 onSelected(contact: Contact) {
   this.selectedContactEvent.emit(contact);
